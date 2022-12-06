567fb8461ed033e4cec7a0e614504c90931af314
Samuel Park <s.park111497@gmail.com>
2022-12-06 00:40:50 -0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
-import React from 'react';
-import Clock, { ClockToggle } from './components/Clock';
-import Folder from './components/Folder';
-import Weather from './components/Weather';
-import AutoComplete from './components/Auto';
+import { useState } from "react";
+import Clock, { ClockToggle } from "./components/Clock";
+import Folder from "./components/Folder";
+import Weather from "./components/Weather";
+import AutoComplete from "./components/Auto";
 
-const names = [
-  'Abba',
-  'Barbara',
-  'Barney',
-  'Jeff',
-  'Jenny',
-  'Sally',
-  'Sarah',
-  'Xander'
-];
+const App = () => {
+  const names = [
+    "Abba",
+    "Barbara",
+    "Barney",
+    "Jeff",
+    "Jenny",
+    "Sally",
+    "Sarah",
+    "Xander",
+  ];
 
-const folders = [
-  { title: 'one', content: 'I am the first' },
-  { title: 'two', content: 'Second folder here' },
-  { title: 'three', content: 'Third folder here' }
-];
+  const folders = [
+    { title: "one", content: "I am the first" },
+    { title: "two", content: "Second folder here" },
+    { title: "three", content: "Third folder here" },
+  ];
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showClock: true
-    };
-  }
-  
-  toggleClock = () => this.setState({ showClock: !this.state.showClock });
-  
-  render () {
-    return (
-      <div className="widgets">
-        <Folder folders={folders} />
-        <Weather />
-        <ClockToggle toggleClock={this.toggleClock} />
-        {this.state.showClock && <Clock />}
-        <AutoComplete names={names} />
-      </div>
-    );
-  }
-}
+  const [showClock, toggleClock] = useState(true);
+
+  return (
+    <div className="widgets">
+      <Folder folders={folders} />
+      <Weather />
+      <ClockToggle toggleClock={() => toggleClock(!showClock)} />
+      {showClock && <Clock />}
+      <AutoComplete names={names} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Auto.js
+++ src/components/Auto.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class AutoComplete extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputVal: '',
-      showList: false
+      inputVal: "",
+      showList: false,
     };
     this.inputRef = React.createRef();
   }
 
   componentDidUpdate() {
     if (this.state.showList) {
-      document.addEventListener('click', this.handleOutsideClick);
+      document.addEventListener("click", this.handleOutsideClick);
     } else {
       console.log("Removing Autocomplete listener on update!");
-      document.removeEventListener('click', this.handleOutsideClick);
+      document.removeEventListener("click", this.handleOutsideClick);
     }
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     console.log("Cleaning up event listener from Autocomplete!");
-    document.removeEventListener('click', this.handleOutsideClick);
+    document.removeEventListener("click", this.handleOutsideClick);
   }
 
   handleInput = (e) => {
     this.setState({ inputVal: e.target.value });
-  }
+  };
 
-  selectName = ({ target:  { innerText: name }}) => {
+  selectName = ({ target: { innerText: name } }) => {
     this.setState({ inputVal: name, showList: false });
-  }
+  };
 
   // Set focus to input field if user clicks anywhere inside the Autocomplete
   // section (unless they have selected a name from the dropdown list)
@@ -39,13 +39,13 @@ class AutoComplete extends React.Component {
     if (!target.classList.contains("nameLi")) {
       this.inputRef.current.focus();
     }
-  }
+  };
 
   handleOutsideClick = () => {
     // Leave dropdown visible as long as input is focused
     if (document.activeElement === this.inputRef.current) return;
     else this.setState({ showList: false });
-  }
+  };
 
   matches = () => {
     const { inputVal } = this.state;
@@ -55,17 +55,17 @@ class AutoComplete extends React.Component {
 
     if (inputLength === 0) return names;
 
-    names.forEach(name => {
+    names.forEach((name) => {
       const nameSegment = name.slice(0, inputLength);
       if (nameSegment.toLowerCase() === inputVal.toLowerCase()) {
         matches.push(name);
       }
     });
 
-    if (matches.length === 0) matches.push('No matches');
+    if (matches.length === 0) matches.push("No matches");
 
     return matches;
-  }
+  };
 
   render() {
     const results = this.matches().map((result) => {
@@ -81,12 +81,12 @@ class AutoComplete extends React.Component {
             {result}
           </li>
         </CSSTransition>
-      )
+      );
     });
 
     return (
-      <section 
-        className="autocomplete-section" 
+      <section
+        className="autocomplete-section"
         onClick={this.handleAutocompleteSectionClick}
       >
         <h1>Autocomplete</h1>
@@ -100,9 +100,7 @@ class AutoComplete extends React.Component {
           />
           {this.state.showList && (
             <ul className="auto-dropdown">
-              <TransitionGroup>
-                {results}
-              </TransitionGroup>
+              <TransitionGroup>{results}</TransitionGroup>
             </ul>
           )}
         </div>
